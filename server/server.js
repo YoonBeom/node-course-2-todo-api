405e171a9ead74928999454af439ffed7bb75fd2
YoonBeom Park <overcam1@gmail.com>
2017-07-16 17:21:50 +0900

    Test POST /todos route

--- server/server.js
+++ server/server.js
@@ -13,6 +13,7 @@ app.post('/todos', (req, res) => {
   var todo = new Todo({
     text: req.body.text
   });
+
   todo.save().then((doc) => {
     res.send(doc);
   }, (e) => {
@@ -23,6 +24,8 @@ app.post('/todos', (req, res) => {
 app.listen(3000, () => {
   console.log('//////// Started on port 3000 ////////');
 });
+
+module.exports = {app};
 
 
 // var newTodo = new Todo({
